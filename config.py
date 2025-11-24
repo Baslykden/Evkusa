ef40c85dc7fb7ce62fe8bba4aa23931cc6eadba3
Baslykden <baslykden@gmail.com>
2025-11-24 19:39:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+# config.py
+from pathlib import Path
+# Токен бота
 BOT_TOKEN = "ВАШ_TELEGRAM_BOT_TOKEN"
-
+# Базовая директория проекта
+# По умолчанию — папка, где лежит сам config.py
+BASE_DIR = Path(__file__).resolve().parent
